aecce3f051d552a3b081d868c4a00b3c47b92e3c
Albert Nguyen <Albert.la.nguyen@gmail.com>
2016-07-18 15:01:26 -0700

    created frontend folder

---
+++ frontend/organ_grinder.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+// window.AppDispatcher = require('./dispatcher/dispatcher.js');
+window.Note = require('./util/notes.js');
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  ReactDOM.render(<div>Hello from organ</div>, root);
+});
